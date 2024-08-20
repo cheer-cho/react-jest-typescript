f16e99759844e61706914ecd4b860422c35400d9
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-20 06:24:23 +0000

    update BombItem to use context

--- src/components/BombPanel/BombList/BombItem/BombItem.tsx
+++ src/components/BombPanel/BombList/BombItem/BombItem.tsx
@@ -1,48 +1,22 @@
-import { Bomb } from "types";
-import BombImage from "../../../../assets/images/bomb-60x60.png";
-import { useEffect, useState } from "react";
-import ListItem from "../../../shared/ListItem";
+import { useEffect } from "react";
+
+import { Bomb } from "@/types";
+import BombImage from "@/assets/images/bomb-60x60.png";
+import ListItem from "@/components/shared/ListItem";
+import { useActions, useTimer } from "@/contexts/BoContext";
 
 type BombItemProps = {
   bomb: Bomb;
-  isStartCountDown: boolean;
-  onBombExploded: () => void;
 };
 
-const BombItem = ({
-  bomb: { initialTime, name },
-  isStartCountDown,
-  onBombExploded: handleBombExploded,
-}: BombItemProps) => {
-  // create local state to track the timer
-  const [timeLeft, setTimeLeft] = useState<number>(initialTime);
-
-  // time countdown start when isStartCountDown = true
+const BombItem = ({ bomb: { initialTime, name } }: BombItemProps) => {
+  const { timer } = useTimer();
+  const { bombExploded } = useActions();
   useEffect(() => {
-    if (isStartCountDown) {
-      // set interval and calculate time left
-      const intervalId = setInterval(() => {
-        setTimeLeft((prevTime) => {
-          if (prevTime <= 1) {
-            // when reach 0 clear the interval (prevent memory leak)
-            clearInterval(intervalId);
-            return 0;
-          }
-          return prevTime - 1;
-        });
-      }, 1000);
-
-      // if component is unmouted (before the timer reach 0)
-      // cleanup interval prevent memory leak
-      return () => clearInterval(intervalId);
+    if (timer === initialTime) {
+      bombExploded();
     }
-  }, [isStartCountDown]);
-
-  useEffect(() => {
-    if (timeLeft === 0) {
-      handleBombExploded();
-    }
-  }, [timeLeft]);
+  }, [timer]);
 
   return (
     <ListItem>
@@ -53,8 +27,8 @@ const BombItem = ({
       <div>
         <span>
           {/* conditional render time left if time > 0 or Exploded if time === 0*/}
-          {timeLeft > 0 ? (
-            `${timeLeft} seconds`
+          {timer < initialTime ? (
+            `${initialTime - timer} seconds`
           ) : (
             <span style={{ color: "red" }}>Exploded</span>
           )}
--- src/components/BombPanel/BombList/BombItem/__tests__/BombItem.test.tsx
+++ src/components/BombPanel/BombList/BombItem/__tests__/BombItem.test.tsx
@@ -1,18 +1,22 @@
 import { act, render, screen } from "@testing-library/react";
 import BombItem from "../BombItem";
+import { useActions, useTimer } from "@/contexts/BoContext";
 
-vi.useFakeTimers();
-const mockedHandleBombExplodedFn = vi.fn();
+// mock Context
+vi.mock("@/contexts/BoContext", () => ({
+  useActions: vi.fn(),
+  useTimer: vi.fn(),
+}));
 
 describe("BombItem Component", () => {
   it("should have correct image, heading and initial time", () => {
-    render(
-      <BombItem
-        isStartCountDown={false}
-        bomb={{ initialTime: 10, name: "Bomb A" }}
-        onBombExploded={() => {}}
-      />
-    );
+    vi.mocked(useTimer).mockReturnValueOnce({ timer: 0 });
+    vi.mocked(useActions).mockReturnValue({
+      bombExploded: vi.fn,
+      startTimer: vi.fn,
+    });
+
+    render(<BombItem bomb={{ initialTime: 10, name: "Bomb A" }} />);
 
     const image = screen.getByRole("img", {
       name: "Bomb A",
@@ -29,40 +33,26 @@ describe("BombItem Component", () => {
   });
 
   it("should counts down correctly when started", () => {
-    render(
-      <BombItem
-        isStartCountDown={true}
-        bomb={{ initialTime: 3, name: "Bomb A" }}
-        onBombExploded={() => {}}
-      />
-    );
-
-    expect(screen.getByText("3 seconds")).toBeInTheDocument();
-
-    act(() => {
-      vi.advanceTimersByTime(1000);
+    // mock timer
+    vi.mocked(useTimer).mockReturnValueOnce({ timer: 2 });
+    vi.mocked(useActions).mockReturnValue({
+      bombExploded: vi.fn,
+      startTimer: vi.fn,
     });
-    expect(screen.getByText("2 seconds")).toBeInTheDocument();
-    act(() => {
-      vi.advanceTimersByTime(1000);
-    });
+    render(<BombItem bomb={{ initialTime: 3, name: "Bomb A" }} />);
+
     expect(screen.getByText("1 seconds")).toBeInTheDocument();
   });
 
   it("should show Exploded word instead of timer, when reaches 0", () => {
-    render(
-      <BombItem
-        isStartCountDown={true}
-        bomb={{ initialTime: 3, name: "Bomb A" }}
-        onBombExploded={mockedHandleBombExplodedFn}
-      />
-    );
-
-    expect(screen.getByText("3 seconds")).toBeInTheDocument();
-
-    act(() => {
-      vi.advanceTimersByTime(3000);
+    const mockedHandleBombExplodedFn = vi.fn();
+    vi.mocked(useTimer).mockReturnValueOnce({ timer: 10 });
+    vi.mocked(useActions).mockReturnValue({
+      bombExploded: mockedHandleBombExplodedFn,
+      startTimer: vi.fn,
     });
+    render(<BombItem bomb={{ initialTime: 10, name: "Bomb A" }} />);
+
     expect(screen.queryByText("seconds")).not.toBeInTheDocument();
     expect(screen.getByText("Exploded")).toBeInTheDocument();
     expect(mockedHandleBombExplodedFn).toHaveBeenCalledOnce();
