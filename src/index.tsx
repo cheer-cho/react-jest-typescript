ec422fe349aa6d2e5b3199c03e1337557b4be9bd
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-20 06:52:12 +0000

    update import statement

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "@/App";
 
 // Create a root.
 const rootElement = document.getElementById("root") as HTMLElement;
